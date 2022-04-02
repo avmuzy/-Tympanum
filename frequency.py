fa9e049d00aca98b679f831b3e534c02bc47714e
Andre Muzy <60109618+avmuzy@users.noreply.github.com>
2022-04-01 21:34:31 -0300

    adding frequency parameters

---
+++ frequency.py
@@ -0,0 +1,2 @@
+import winsound
+winsound.Beep(1000, 1000)
--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 import pygame
 pygame.init()
-pygame.mixer.music.load('Thunder.mp3')
+pygame.mixer.music.load('x1.mp3')
 pygame.mixer.music.play()
 pygame.event.wait()
